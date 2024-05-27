d1747bf5802f86f1323858ad18c3722662957c4f
MridulSheikh <prince9nazir@gmail.com>
2024-05-27 20:06:00 +0600

    update product details ui

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -6,14 +6,22 @@ const ProductDetails = () => {
   const { brand, description, image_url, price, title } = shoe;
 
   return (
-    <div>
-      <h1 className="text-5xl text-center font-bold">{title}</h1>
+    <div className=" flex flex-wrap lg:flex-nowrap gap-x-10 max-w-screen-xl mx-auto px-5 mt-10">
+      <div className=" relative h-[600px]">
+        <img className=" h-full" src={image_url} alt="product image" />
+      </div>
+      <div>
+        <h1 className="text-5xl font-bold">{title}</h1>
 
-      <img className="h-[600px]" src={image_url} alt="product image" />
-
-      <h3 className="text-2xl font-semibold">{price} $$</h3>
-      <h3 className="text-xl font-semibold">{brand}</h3>
-      <p className="text-lg font-light">{description}</p>
+        <h3 className="text-2xl mt-5 font-bold">$ {price}</h3>
+        <h3 className="text-xl font-semibold mt-5">{brand}</h3>
+        <p className="text-lg font-normal text-gray-500 mt-5">{description}</p>
+        <div className="mt-10">
+          <button className="btn bg-[#d0021b] text-md px-14 uppercase py-3 rounded-full font-semibold text-white">
+            Add to cart
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
